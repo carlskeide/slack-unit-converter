f8b942cdca10df26736e70d06929ba467d9c3775
Carl Skeide <carl@skeide.se>
2020-11-18 01:30:42 +0000

    Make path prefix configurable

--- src/config.py
+++ src/config.py
@@ -10,6 +10,6 @@ DEBUG = False
 REDIS_URL = sec.load("redis_url", fallback="redis://localhost")
 
 # Flack
-FLACK_URL_PREFIX = "/"
 FLACK_DEFAULT_NAME = "UnitConverter"
 FLACK_TOKEN = sec.load("slack_token")
+FLACK_URL_PREFIX = sec.load("flack_prefix", fallback="/")
